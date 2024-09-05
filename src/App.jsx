5c0aab6502b03b499ef9e983b45d2dbcf6199619
matevszz <mateusz.dobiecki06@gmail.com>
2024-09-05 19:50:05 +0200

    adding button and functions

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
     <Navbar />
       <div className="all-app">
         <h1>ready-ui</h1>
+        <p>ready-ui is ready-to-use ui library to increase speed of creating projects</p>
+        <p>all library is free to use and updating so use it if you want</p>
       </div>
     </>
   )
--- src/pages/Design.jsx
+++ src/pages/Design.jsx
@@ -1,10 +1,29 @@
 import './Design.css';
 import Navbar from '../Components/Navbar';
 
+const copyButton = (onClick) => (
+  <buttton className="copyBtn" onClick={onClick}>Copy</buttton>
+)
+
+function copyToClipboard (cssParam, type) {
+  navigator.clipboard.writeText(cssParam).then(alert(`You just copied ${type} design!`));
+}
+
+const classicButtonDesign = 'test';
+
 function Design () {
   return (
     <>
         <Navbar />
+        <div className="design-app">
+          <div className="buttons">
+            <div className="classic-button">
+              <p>✅ classic button</p>
+              <button className='classic-buttonBtn'></button>
+              {copyButton(() => {copyToClipboard(classicButtonDesign, 'button')})}
+            </div>
+          </div>
+        </div>
     </>
   )
 }
