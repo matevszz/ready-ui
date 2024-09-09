b737da464435a4f24720f4863760ac6a414171a8
matevszz <mateusz.dobiecki06@gmail.com>
2024-09-09 17:46:41 +0200

    added css parameters

--- src/pages/Design.jsx
+++ src/pages/Design.jsx
@@ -13,7 +13,39 @@ function copyToClipboardButton (cssParam, type) {
 
 
 //classic button design
-const classicButtonCopy = 'test';
+const classicButtonCopy = `
+    font-size: 24px;
+    padding: 24px 48px;
+    border-radius: 50px;
+    border: 1px solid #212121;
+    cursor: pointer;
+    font-weight: 500;
+    background: rgba(0, 0, 0, .05);
+    color: #212121;
+    transition: all .3s;
+    
+    !!! while hover !!!
+    
+    background: #212121;
+    color: white;`;
+
+const retroButtonCopy = `
+    font-size: 24px;
+    padding: 24px 48px;
+    background: rgba(0, 0, 0, .05);
+    border: 1px solid #212121;
+    cursor: pointer;
+    border-radius: 15px;
+    box-shadow: 5px 5px 0px black;
+    font-weight: 400;
+    transition: all .1s;
+    
+    !!! while hover !!!
+    
+    box-shadow: 10px 10px 0px black;
+    transform: translateY(-5px);
+    font-weight: 600;
+    border: 2px solid #212121;`
 
 function Design () {
   
@@ -30,7 +62,7 @@ function Design () {
             <div className="retro-button">
               <p>📺 retro button</p>
               <button className='retro-buttonBtn'>Button</button>
-              {copyButton(() => {copyToClipboardButton(classicButtonCopy, 'button')})}
+              {copyButton(() => {copyToClipboardButton(retroButtonCopy, 'button')})}
             </div>
           </div>
         </div>
